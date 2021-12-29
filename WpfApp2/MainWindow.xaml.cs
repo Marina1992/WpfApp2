f5fab4e005b8696867168e29713f2e9a868b11bc
Марина Разумова <marina.razumova.19@yandex.ru>
2021-12-29 19:26:17 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double num1 = 0;// поле (1 число)
+        double num2 = 0;// поле (2 число)
+        string op = "";// операция, которую ввел пользователь  
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_num_Click(object sender, RoutedEventArgs e)
+        {
+
+            Button button = (Button)sender;
+            String num = button.Content.ToString();
+
+            if (txtValue.Text == "0")
+                txtValue.Text = num;
+            else
+                txtValue.Text += num;
+
+
+            if (op =="") // если операция отсутствовала, то работали с num1
+           
+            {
+
+                num1 = Double.Parse(txtValue.Text);
+            }
+
+            else
+            {
+                num2 = Double.Parse(txtValue.Text);
+
+
+            }
+        }
+
+       
+        private void btn_op_Click(object sender, RoutedEventArgs e)
+        {
+
+            Button button = (Button)sender;
+            op = button.Content.ToString();
+            txtValue.Text = "0";
+        }
+
+        private void btn_eq_Click(object sender, RoutedEventArgs e)
+        {
+            double result = 0;
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+
+                case "-":
+                    result = num1 - num2;
+                    break;
+
+                case "√x":
+                    result = Math.Sqrt(num1);
+                    break;
+
+                case "1/x":
+                    result = 1/num1;
+                    break;
+
+                case "%":
+                    result = num1* num2/100;
+                    break;
+
+                case "x^2":
+                    result = Pow(num1);
+                    break;
+            }
+
+            txtValue.Text = result.ToString();
+            op = ""; // обнулить операцию
+            num1 = result;
+            num2 = 0;
+        }
+
+        private double Pow(double x )
+        {
+
+            double result = 1;
+            for (int i = 1; i <= 2; i++)
+            {
+                result = result * x;
+            }
+            return result;
+        }
+
+        private void btn_C_Click(object sender, RoutedEventArgs e)
+        {
+            num1 = 0;
+            num2 = 0;
+            op = "";
+            txtValue.Text = "0";
+        }
+
+        private void btn_CE_Click(object sender, RoutedEventArgs e)
+        {
+            if ( op =="")
+            {
+                num1 = 0;
+            }
+            else
+            {
+                num2 = 0;
+            }
+            txtValue.Text = "0";
+        }
+
+        private void btn_backspc_Click(object sender, RoutedEventArgs e)
+        {
+            txtValue.Text = DropLastChar(txtValue.Text);
+            if(op=="")
+            {
+                num1 = Double.Parse(txtValue.Text);
+               
+
+            }
+
+            else
+            {
+                num2 = Double.Parse(txtValue.Text);
+                
+            }
+        }
+
+        private string DropLastChar(string text)
+        {
+            if (text.Length == 1)
+                text = "0";
+
+            else
+            {
+                text = text.Remove(text.Length - 1, 1);
+                if (text[ text.Length-1]== ',')
+                
+                    text = text.Remove(text.Length - 1, 1);
+                    
+            }
+            return text;
+        }
+
+        private void btn_plusminus_Click(object sender, RoutedEventArgs e)
+        {
+            if (op == "")
+            {
+                num1 = num1 * -1;
+                txtValue.Text = num1.ToString();
+            }
+            else
+            {
+                num2 = num2 * -1;
+                txtValue.Text = num2.ToString();
+
+            }
+        }
+
+        private void btn_comma_Click(object sender, RoutedEventArgs e)
+        {
+            if (op == "")
+                SetComma(num1);
+            else
+                SetComma(num2);
+        }
+
+        private void SetComma(double num)
+        {
+           
+
+          if (txtValue.Text.Contains (','))
+                return;
+
+            txtValue.Text+=',' ;
+        }
+    }
+}
